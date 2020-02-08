3c73ab5483117a55f5afbe04d1ae0e65e8e3720d
mike roshak <roshak@hotmail.com>
2020-02-07 16:39:29 -0800

    test

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ import requests
 res = requests.post('http://localhost/device', json={"name":"lalala"})
 if res.ok:
     print (res.json())
+
+res = requests.post('http://localhost/device', json={"name":"lalala"})
